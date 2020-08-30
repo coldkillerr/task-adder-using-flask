c6d967cc56158139c33a8e32287f8e1c89c1e95d
coldkillerr <yonishitjain123456@gmail.com>
2020-08-30 11:48:56 +0530

    added base.html , index.html,about.html

--- app.py
+++ app.py
@@ -11,8 +11,11 @@ app=Flask(__name__)
 @app.route('/') #this is the decorator to the function index()
 @app.route('/index') # you can add more than one routes 
 def index():
-    return render_template('index2.html',current_title='NEW TITLE') # here you can add the required html and pass values to the html . We can use {{ }} to use passed values in the html file
+    return render_template('index.html',current_title='NEW TITLE') # here you can add the required html and pass values to the html . We can use {{ }} to use passed values in the html file
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
 if __name__=='__main__' :
 
     app.run(debug=True)
